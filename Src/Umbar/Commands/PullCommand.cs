7418081352b60f4c997ffa4b00deeabcac7916a8
Bjørn Rasmussen <bjr@matas.dk>
2024-12-12 13:46:18 +0100

    Updated Pull command to be able to select multiple apps to pull image from Added option to mark all Added option to force

--- Src/Umbar/Commands/PullCommand.cs
+++ Src/Umbar/Commands/PullCommand.cs
@@ -8,25 +8,59 @@ using System.ComponentModel;
 namespace Umbar.Commands;
 
 [Description("Pulls images from one or more apps from the config.json")]
+public sealed class PullCommand : AsyncCommand<PullSettings>
 {
-    public override async Task<int> ExecuteAsync(CommandContext context, DefaultSettings settings)
+    public override async Task<int> ExecuteAsync(
+        CommandContext context,
+        PullSettings settings)
     {
         var config = await ConfigurationManager.GetAsync();
-        if (config.Applications.Count == 0)
+        if (config.Apps.Count == 0)
         {
-            AnsiConsole.WriteLine("No apps added");
+            AnsiConsole.WriteLine("No apps added.");
             return 0;
         }
         var currentDirectory = Directory.GetCurrentDirectory();
-        App? app = config.Applications.FirstOrDefault(a => currentDirectory.Contains(a.Name));
-        if (app == null)
+        var directoryApp = config.Apps.FirstOrDefault(a => currentDirectory.Contains(a.Name));
+        HashSet<App>? apps = directoryApp == null ? [] : [directoryApp];
+        if (apps.Count == 0 || settings.All)
         {
-            AnsiConsole.WriteLine("Please select which app to pull");
-            app = await config.Applications.SelectionPromptAsync(a => a.Name);
+            apps = await Prompt(config, settings);
         }
-        var output = await ProcessHelper.RunAsync(Constants.Docker, $"compose -f {app.Path} pull");
+        if (apps.Count == 0)
+        {
+            AnsiConsole.WriteLine("No apps were added.");
+            return 1;
+        }
+        foreach (var app in apps)
+        {
+            await ProcessHelper.RunAsync(Constants.Docker, $"compose -f {app.Path} pull");
+        }
 
         await ConfigurationManager.UpdateAsync(config);
         return 0;
     }
+    private static async Task<HashSet<App>> Prompt(
+        Config config,
+        PullSettings settings)
+    {
+        if (settings.Force && settings.All)
+            return [.. config.Apps];
+
+        AnsiConsole.WriteLine("Please select which app to pull");
+        if (settings.All)
+        {
+            return (await config.Apps.MultiSelectionPromptAsync(true)).ToHashSet();
+        }
+        return [await config.Apps.SelectionPromptAsync(a => a.Name)];
+    }
+}
+public sealed class PullSettings : DefaultSettings
+{
+    [CommandOption("-a|--all")]
+    [Description("Marks all apps in the multi selection")]
+    public bool All { get; set; }
+    [CommandOption("-f|--force")]
+    [Description("Skips the multi selection")]
+    public bool Force { get; set; }
 }
